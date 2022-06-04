ec991c142f7219d64696c779745b666d138c1567
disco <this.disco.w@gmail.com>
2022-06-04 11:47:21 +0300

    added grid selection by click

--- Assets/Scripts/Grid/GridConstructor.cs
+++ Assets/Scripts/Grid/GridConstructor.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.IO;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class GridConstructor : MonoBehaviour
+public class GridConstructor : MonoBehaviour, IPointerDownHandler
 {
     [SerializeField] private RectTransform _rectTransform;
     [SerializeField] private GridLayoutGroup _gridLayoutGroup;
@@ -24,6 +26,16 @@ public class GridConstructor : MonoBehaviour
 
     private int _rowsCount;
     [Space] [SerializeField] private string _levelName;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (Input.GetMouseButton(0))
+        {
+#if UNITY_EDITOR
+            Selection.activeObject = gameObject;
+#endif
+        }
+    }
 
     #region Constructor Navigation
 
