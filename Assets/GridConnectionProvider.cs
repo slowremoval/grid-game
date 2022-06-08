14f121d1f459606c7d945a60078bc18fc2a454df
disco <this.disco.w@gmail.com>
2022-06-08 22:43:17 +0300

    added grid connections lerp

--- Assets/GridConnectionProvider.cs
+++ Assets/GridConnectionProvider.cs
@@ -1,23 +1,25 @@
+using System.Collections;
 using System.Collections.Generic;
 using GridField.Cells;
 using UnityEngine;
 
 public class GridConnectionProvider : MonoBehaviour
 {
-    
     //top - 0
     //down - 1
     //left - 2
     //right - 3
 
-    [SerializeField]SimpleCell _simpleCell;
-    
-    
+    [SerializeField] SimpleCell _simpleCell;
+
+
     [Header("Placeholder connections")] [Space] [SerializeField]
     private List<GameObject> _placeholderConnections;
 
     [Header("Cell item connections")] [Space] [SerializeField]
     private List<GameObject> _cellItemrConnections;
+
+    private float _connectionScaleSpeed;
 
     private void Start()
     {
@@ -31,8 +33,25 @@ public class GridConnectionProvider : MonoBehaviour
         for (var index = 0; index < _placeholderConnections.Count; index++)
         {
             GameObject connectionSide = _placeholderConnections[index];
-            
-            connectionSide.SetActive(neighbours[index]);
+            connectionSide.transform.localScale = Vector3.zero;
+            if (neighbours[index])
+            {
+                connectionSide.SetActive(true);
+                StartCoroutine(ActivateConnectionRoutine(connectionSide));
+            }
+        }
+    }
+
+    private IEnumerator ActivateConnectionRoutine(GameObject connectionSide)
+    {
+        while (connectionSide.transform.localScale.x < 1)
+        {
+            yield return null;
+
+            _connectionScaleSpeed = 5f;
+            connectionSide.transform.localScale =
+                Vector3.Lerp(connectionSide.transform.localScale, new Vector3(1.05f, 1, 1),
+                    Time.deltaTime * _connectionScaleSpeed);
         }
     }
 }
